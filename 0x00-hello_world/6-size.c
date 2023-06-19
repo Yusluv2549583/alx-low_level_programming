c251dcf113ad9b08440787d79850eb90a34a03d9
yusluv2549583 <lawalyussuf583@gmail.com>
2023-06-19 07:48:15 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point
  *
@@ -11,10 +13,11 @@ int main(void)
 	long num1;
 	long long num2;
 	float num3;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
 	printf("Size of an int: %zu byte(s)\n", sizeof(num));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(num1));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(num2));
 	printf("Size of a float: %zu byte(s)\n", sizeof(num3));
 	return (0);
-}	
+}
