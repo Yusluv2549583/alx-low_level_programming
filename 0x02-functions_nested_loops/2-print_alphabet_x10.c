01fe221afdb0eebe9610054c6c9b1fce8e9167c9
yusluv2549583 <lawalyussuf583@gmail.com>
2023-06-21 16:45:58 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,18 @@
  */
 int main(void)
 {
-	    char letter;
-	    int i;
-	    for (i = 0; i < 10; i++)
-	    {
-		    for (letter = 'a'; letter <= 'z'; letter++
+	char letter;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++
 )
-		    {
-			    _putchar(letter);
-		    }
-	    }
-	    _putchar('\n');
+		{
+			_putchar(letter);
+		}
+	}
+
+	_putchar('\n');
+	return (0);
 }
 
